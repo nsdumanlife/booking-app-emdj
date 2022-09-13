26f01e115abd8a3947de272f9fb3088bc1a86378
NSDuman <numanselmanduman@gmail.com>
2022-09-13 21:03:29 +0000

    Create tests

---
+++ test/get-duration.test.js
@@ -0,0 +1,48 @@
+const getDuration = require('../src/helper/get-duration')
+
+describe('getDuration function', () => {
+  test('gets time as a day between given two date objects', () => {
+    const startDate = new Date('09/13/2022')
+    const endDate = new Date('09/15/2022')
+
+    const result = getDuration(startDate, endDate)
+
+    expect(result).toBe(2)
+  })
+
+  test('gets time as a day between given two strings should throw error', () => {
+    const startDate = '09/13/2022'
+    const endDate = '09/15/2022'
+
+    // const result = getDuration(startDate, endDate)
+
+    expect(() => getDuration(startDate, endDate)).toThrow(TypeError)
+  })
+
+  test('if startDate > endDate it should throw error', () => {
+    const startDate = '09/17/2022'
+    const endDate = '09/15/2022'
+
+    // const result = getDuration(startDate, endDate)
+
+    expect(() => getDuration(startDate, endDate)).toThrow(Error)
+  })
+
+  test('if startDate is invalid date should throw error', () => {
+    const startDate = '09/54/2022'
+    const endDate = '09/15/2022'
+
+    // const result = getDuration(startDate, endDate)
+
+    expect(() => getDuration(startDate, endDate)).toThrow(Error)
+  })
+
+  test('if endDate is invalid date should throw error', () => {
+    const startDate = '09/15/2022'
+    const endDate = '09/54/2022'
+
+    // const result = getDuration(startDate, endDate)
+
+    expect(() => getDuration(startDate, endDate)).toThrow(Error)
+  })
+})
